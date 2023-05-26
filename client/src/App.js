9233df83e66becce38d8511888ae65d2df27817e
Shivam Kumar <shivkumar386112@gmail.com>
2023-05-26 12:16:00 +0530

    initiated project

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Dashboard from './components/Dashboard';
+import LandingPage from './components/LandingPage';
+
+function App() {
+  return (
+    <>
+    <h1>Hello react!</h1>
+    <Dashboard/>
+    <LandingPage/>
+    </>
+  );
+}
+
+export default App;
